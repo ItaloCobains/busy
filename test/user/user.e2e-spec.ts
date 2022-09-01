1574ebac86b2692229387615e2825dee2fe9cdad
ItaloCobains <monokatchaleger@gmail.com>
2022-09-01 10:03:55 -0300

    create more configs for connect test on db

--- test/user/user.e2e-spec.ts
+++ test/user/user.e2e-spec.ts
@@ -2,13 +2,26 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { UserModule } from '../../src/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 describe('User: /user', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [UserModule],
+      imports: [
+        UserModule,
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5433,
+          username: 'postgres',
+          password: 'docker',
+          database: 'testdb',
+          autoLoadEntities: true,
+          synchronize: true,
+        }),
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -20,5 +33,4 @@ describe('User: /user', () => {
   });
 
   it.todo('Create POST /user');
-  
 });
