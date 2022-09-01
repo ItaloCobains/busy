16c5e030c2a0f9c7d9c02a98484c5c7f321ef965
ItaloCobains <monokatchaleger@gmail.com>
2022-09-01 13:36:34 -0300

    fix a bug

--- test/user/user.e2e-spec.ts
+++ test/user/user.e2e-spec.ts
@@ -1,9 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-<<<<<<< HEAD
-import { INestApplication, ValidationPipe } from '@nestjs/common';
-=======
-import { HttpStatus, INestApplication } from '@nestjs/common';
->>>>>>> release/1.6.0
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { UserModule } from '../../src/user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -11,18 +7,10 @@ import { CreateUserDto } from '../../src/user/dto/create-user-dto';
 
 describe('User: /user', () => {
   let app: INestApplication;
-
-<<<<<<< HEAD
-  const user = {
-    name: 'ItaloCobains',
-    email: 'italo@gmail.com',
-    password: 'italo12345',
-=======
   const user: CreateUserDto = {
     name: 'Italo',
     email: 'italo@gmail.com',
     password: 'ITALO12345',
->>>>>>> release/1.6.0
     avatar: 'http://avatar.com',
   };
 
