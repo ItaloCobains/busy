54bd7265e25fb3487343e268da196ce9e7e13d75
ItaloCobains <monokatchaleger@gmail.com>
2022-09-02 13:29:24 -0300

    reafatoring

--- src/database.providers.ts
+++ src/database.providers.ts
@@ -1,6 +1,7 @@
 import { DataSource } from 'typeorm';
 import * as path from 'path';
 import 'reflect-metadata';
+import { CreateUserTable1661945684451 } from './migrations/1661945684451-CreateUserTable';
 
 export const databaseProviders = [
   {
@@ -21,3 +22,15 @@ export const databaseProviders = [
     },
   },
 ];
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: 'db',
+  port: 5432,
+  username: 'postgres',
+  password: 'docker',
+  database: 'nodeapp',
+  entities: [__dirname + '/../**/*.entity.js'],
+  synchronize: false,
+  migrations: [CreateUserTable1661945684451]
+});
